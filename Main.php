fb1f9c07b2ca81894f5378cd8ab0812b079c2396
sergey96 <altronxr@gmail.com>
2023-01-12 11:07:55 +0200

    create new project

---
+++ Main.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Auto.css">
+    <link rel="stylesheet" href="pg_Adapt.css">
+    <title>Auto-shop</title>
+</head>
+<body>
+
+
+<div class="Main_page flex_mod">
+<img id="btn_up" src="media/strUP_1.png" width="45" height="45" alt="" style="z-index: 15;">
+
+<div class="Block1">
+
+<div class="fon">
+<div class="nav_menu_fon"></div>
+<aside>
+<img id="close_blMenu" src="media/wClose.png" width="30" height="30"alt="">
+<img id="l_blMenu_pg" src="media/logo.svg" width="160" height="160"alt="">
+
+<div class="cotacts_posit flex_mod">
+<ul>
+ <a href=""><li>Каталог</li></a>
+ <a href=""><li>Цены</li></a>
+ <a href=""><li>Сервис центы</li></a>
+ <a href=""><li>Страховка</li></a>
+ <a href=""><li>О нас</li></a>
+</ul>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81276.30065487922!2d30.28342968750923!3d50.46187738664641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cca4fe799a0b%3A0xac1d11c607591b32!2zSGVsZW4gRG9yb24gRW5nbGlzaCDQkNC90LPQu9GW0LnRgdGM0LrQsCDQtNC70Y8g0LTRltGC0LXQuQ!5e0!3m2!1sru!2sua!4v1656237298024!5m2!1sru!2sua"
+ width="100%" height="180" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<div class="nav_menu_contacts">
++38095544656 - Менеджер<br/>
+04455685566 - гл. Офис<br/>
+г.Киев, ул.Булаховского 30а, кв.113
+</div>
+
+</div>
+</aside>
+
+
+  
+  <img id="logo" src="media/logo.svg" width="200" height="200" alt="">
+  <img id="logMenu22" src="media/menu.png" alt="">
+
+<div class="contacts">
+  <div>
+  <p>
+    <b>Наши контакты:</b><br/>
+    +38065545453 -- aМенеджер<br/>
+    +38095654566 -- Директор<br/>
+    +04478678766 -- Офис<br/>
+  </p>
+  </div>
+
+  <nav class="link_soc flex_mod">
+  <img id="" src="media/card2.png" alt="">
+  <img id="" src="media/card3.png" alt="">
+  <img id="" src="media/card4.png" alt="">
+  </nav>
+</div>
+
+<section class="wrap_block_car">
+  <div class="block_car flex_mod">
+    <div class="text_car">
+   <small>Встречайте!</small> <br/>
+  <small>Новинка этого года:</small><br/>
+   <b>Mercedes-Benz C-Class</b></div>
+    <img id="big_foto_mers" src="media/carMers.png" alt="">
+  </div>
+  <div class="block_info_car flex_mod">
+    <div class="wz1 wrap_znack"> <img src="media/cuzov.png" alt=""> <p>Объем бака: 66 л</p> </div>
+    <div class="wz2 wrap_znack"> <img src="media/speed.png" alt=""> <p>450 км/ч</p> </div>
+    <div class="wz3 wrap_znack"> <img src="media/dvig.png" alt=""> <p> C450 AMG 4MATIC</p> </div>
+    <button id="btn_back" type="button"> Заказать Test-Drive </button>
+  </div>
+
+
+  <div class="DOP_block_info_car">
+    <div class="znak">
+    <div class="DOP_znack flex_mod"> <img src="media/cuzov.png" alt=""> <p>Объем бака: 66 л</p> </div>
+    <div class="DOP_znack flex_mod"> <img src="media/speed.png" alt=""> <p>450 км/ч</p> </div>
+    <div class="DOP_znack flex_mod"> <img src="media/dvig.png" alt=""> <p> C450 AMG 4MATIC</p> </div>
+    </div>
+    <button id="btn_back2" type="button"> Заказать Test-Drive </button>
+  </div>
+</section>
+
+<img id="btn_down" src="media/down.png" width="40" height="40" alt="">
+</div>  
+</div>
+
+
+
+<div class="Block_video">
+<div class="video_Mers">
+<video loop muted src="media/vid_mers.mp4" type="video/mp4" autoplay > </video>
+</div>
+</div>
+
+
+
+
+<div class="Block_2">
+<h1 class="h_1">Новые технологии в деталях</h1>
+
+<div class="Slider_block">
+  <div class="slidelist">
+      <div class="slide s1"> <img id="foto_car_1" src="media/mers1.jpg" alt=""> 
+        <p>Как и многие иные модели торговой марки Mercedes-Benz автомобили C-класса 
+        имеют высокопроизводительные модификации от подразделения Mercedes-AMG. </p> 
+      </div>
+
+      <div class="slide"> <img id="foto_car_2" src="media/mers2.jpg" alt=""></div>
+
+      <div class="slide"> <img id="foto_car_3" src="media/mers4.webp" alt="">
+      <p name="tx_1">Лучший автомобиль в своём классе 2014 года по версии Euro NCAP. 
+        Всемирный автомобиль 2015 года в конкурсе «World Car of the Year»</p>
+      </div>
+  </div>
+</div>
+
+</div>
+</div>
+<!---------------------- Main END --------------------->
+
+
+
+
+
+<div class="Main_page_2 flex_mod">
+
+<div class="Block_3 flex_mod">
+<section class="mini_block_3">
+  <div class="mini_block_left flex_mod">    
+   <img id="mers_dvig" src="media/mers.png" alt="">
+   <p> <span>Обновленный V8 двигатель</span><br/><br/>
+   Введение новой разработки было постепенным и началось с осени того же года. 
+   Для моделей S-класса 2014 года (W222) мощность была увеличена до 455 л.с. (335 кВт) при 5250 оборотах в минуту, 
+   с сохранением крутящего момента в 700 Н·м при 1800-3500 об/мин.
+   Двигатель M278 представляет собой V8 бензиновый агрегат с углом развала цилиндров 90° и 
+   непосредственным впрыском топлива в цилиндры (CGI). Он происходит от предыдущего представителя серии M273, 
+   разделяя с ним такие характеристики и элементы, как диаметр поршней, алюминиевый блок двигателя и гильзы цилиндров 
+   с низким коэффициентом трения из алюминия и кремния</p>
+  </div>
+
+    <div class="mini_block_right flex_mod">
+      <img id="mers_salon" src="media/inside_bmw.jpg" alt="">
+      <p><span>Интерактивный салон</span><br/><br/>
+      Передние сиденья с функцией памяти могут запоминать ваше удобное положение, а задние пассажиры 
+      имеют еще больше места для ног. Это и многие другие особенности комфорта делают новый C-Class вашей личной зоной комфорта.
+      А новое поколение инновационной мультимедийной системы MBUX сделает вождение нового седана C-Class 
+      цифровым и интуитивно понятным. Обновления включают в себя интеллектуальные функции, 
+      такие как дополненная реальность для навигации.
+    </p>
+    </div>
+</section>
+
+<section class="fara_block">
+  <p>Изысканный дизайн </p>
+  <img id="mers_fara" src="media/10.jpeg" alt="">
+  <p name="textfara">Cередины 2018 года выпускается версия с двумя моторами (AWD, от $54 тыс., 0—100 км/ч за 4,5 с) 
+    и версия с улучшенной производительностью (Performance AWD, от $64 тыс., 0—100 км/ч за 3,5 с). 
+    Обе версии комплектуются «премиальным пакетом» и батареей увеличенной ёмкости и имеют запас хода в 310 миль(500 км)
+  </p>
+</section>
+
+</div>
+
+
+
+
+<div class="Block_4 flex_mod">
+<h1 class="h_2">Другие модели </h1>
+<section class="wrap_slider">
+  <div class="slider_line">
+    <div class="block bl1 flex_mod"><img id="foto1" src="media/99.png" alt=""><p>Mercedes S–Class Cabriolet</p><button class="btn1 btnmod">Купить</button></div>
+    <div class="block bl2 flex_mod"><img id="foto2" src="media/mers_black.jpg" alt=""><p>Mercedes C-Class</p> <button class="btn2 btnmod">Купить</button> </div>
+    <div class="block bl3 flex_mod"><img id="foto3" src="media/77.png" alt=""><p>Mercedes GLC AMG Coupe</p><button class="btn3 btnmod">Купить</button></div>
+  </div>
+</section>
+<div class="btn_group">
+<img class="btn_prev" src="media/prev.png" alt="">  
+<img class="btn_next" src="media/next.png" alt="">  
+</div>
+</div>
+
+
+
+
+<footer class="Block_foter flex_mod">
+
+<div class="bl_footer_1">
+<section class="DOP_footer">
+<input type="checkbox"  id="up">
+<label id="label_menu" for="up">Меню &#9661;</label>
+<div class="DOP_bl_1">
+<ul>
+    <li>Каталог</li>
+    <li>Клиентам</li>
+    <li>Карьера</li>
+    <li>Наши Сервис центры</li>
+  </ul>
+</div>  
+
+<input type="checkbox"  id="down">
+<label id="label_contacts" for="down">Контакты &#9661;</label>
+<div class="DOP_bl_2">
+<ul>
+    <li> +38050667844545 - Менеджер</li>
+    <li> +38050667844545 - гл. Офис</li>
+    <li>+38050667844545 - Директор</li>
+    <li>Наши Сервис центры</li>
+  </ul>
+</div>  
+</section>
+
+
+
+<div class="foter_menu">
+  <p>Меню</p>
+  <ul>
+    <li>Каталог</li>
+    <li>Клиентам</li>
+    <li>Карьера</li>
+    <li>Наши Сервис центры</li>
+  </ul>
+</div>
+
+<div class="footer_contacts">
+<p>Связаться с нами:</p>
+<ul>
+    <li> +38050667844545 - Менеджер</li>
+    <li> +38050667844545 - гл. Офис</li>
+    <li>+38050667844545 - Директор</li>
+    <li>Наши Сервис центры</li>
+  </ul>
+</div>
+
+<div class="bl_foto_1">
+<img id="logo_footer2" src="media/logoMers.png" alt="">
+<img id="logo_footer1" src="media/logo_footer.svg" alt="">
+</div>
+</div>
+
+
+<section class="bl_footer_2">
+<p>Auto-World | 2022г.</p>
+<p>Все права защищены.</p>
+<div class="bl_foto_2">
+<img src="media/viza.svg" alt="">
+<img id="bank2" src="media/master.png" alt="">
+</div>
+</section>
+
+</footer>
+
+</div>
+<!---------------------END Main_page_2 ---------------------------->
+
+<!-- <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script> -->
+<script src="Auto.js"></script>
+</body>
+</html>
